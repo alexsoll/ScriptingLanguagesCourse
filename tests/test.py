740d6c4413dc4db394e6dc3d811d7ed919c8cb11
alexsoll <al.soluyanov@rambler.ru>
2021-03-13 22:33:12 +0300

    test: add initial tests

---
+++ tests/test.py
@@ -0,0 +1,22 @@
+import unittest
+from Polynomial.polynomial import Polynomial
+
+class TestPolynomialMethods(unittest.TestCase):
+    def test_emtpy_args_constructor(self):
+        self.assertRaises(TypeError, Polynomial)
+
+    def test_emtpy_list_constructor(self):
+        self.assertRaises(ValueError, Polynomial, [])
+
+    def test_emtpy_tuple_constructor(self):
+        self.assertRaises(ValueError, Polynomial, ())
+
+    def test_incorrect_types_constructor(self):
+        self.assertRaises(TypeError, Polynomial, 1.0)
+
+    def test_incorrect_lists_elements_constructor(self):
+        self.assertRaises(TypeError, Polynomial, ["1", "2"])
+
+
+if __name__ == '__main__':
+    unittest.main()
