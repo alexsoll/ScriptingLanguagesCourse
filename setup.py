70ce185f81a35d2cd0afef28db5c7c3be851f1f7
alexsoll <al.soluyanov@rambler.ru>
2021-03-13 22:32:42 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='polynomial',
+      version='0.1',
+      description='Polynomial class implementation',
+      author='Alexey S.',
+      packages=['Polynomial'],
+      zip_safe=False
+)
