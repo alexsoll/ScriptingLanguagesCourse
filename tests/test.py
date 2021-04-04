7f691117d83df2a1675d9c2beb7825c1b1dad17c
alexsoll <al.soluyanov@rambler.ru>
2021-04-04 20:21:20 +0300

    tests: add new tests

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,4 @@
 import unittest
-import traceback
 from Polynomial.polynomial import Polynomial
 
 
@@ -20,14 +19,21 @@ class TestPolynomialMethods(unittest.TestCase):
         self.assertRaises(TypeError, Polynomial, ["1", "2"])
 
     def test_polynomial_to_str(self):
-        # Test 1
-        self.assertEqual(Polynomial([1, -1, 1]).__str__(), "x^2-x+1")
-        
-        # Test 2
-        self.assertEqual(Polynomial([-1, 2, 0]).__str__(), "-x^2+2x")
+        params = [
+            [1, -1, 1],
+            [-1, 2, 0],
+            [5],
+            [1, 0, 1],
+            [1, 0, 1, 1],
+            [1, 0, -1, 1],
+            [0, 1]
+        ]
 
-        # Test 3
-        self.assertEqual(Polynomial([5]).__str__(), "5")
+        results = [
+            "x^2-x+1", "-x^2+2x", "5", "x^2+1", "x^3+x+1", "x^3-x+1", "1"
+        ]
+        for i, param in enumerate(params):
+            self.assertEqual(Polynomial(param).__str__(), results[i], f"Incorrect comparison for {Polynomial(param).__repr__()}")
 
     def test_repr(self):
         # Test 1
@@ -73,6 +79,157 @@ class TestPolynomialMethods(unittest.TestCase):
 
         # Test 2
         self.assertEqual([1], p.coeffs)
+
+    def test_compare(self):
+        # Initialization
+        p1 = Polynomial([1, 2, 3])
+        p2 = Polynomial([1, 2])
+
+        # Test 1
+        self.assertNotEqual(p1, p2)
+
+        p2.coeffs.append(3)
+
+        # Test 2
+        self.assertEqual(p1, p2)
+
+        p2.coeffs = [0, 1, 2, 3]
+
+        # Test 3
+        self.assertEqual(p1, p2)
+
+        p2.coeffs = [1, 2, 3, 0]
+
+        # Test 4
+        self.assertNotEqual(p1, p2)
+
+    def test_addition_two_polynomials(self):
+        # Initialization
+        p1 = Polynomial([1, 2, 3])
+        p2 = Polynomial([1, 2])
+
+        # Addition
+        res1 = p1 + p2
+        res2 = p2 + p1
+
+        self.assertEqual(res1.coeffs, [1, 3, 5])
+        self.assertEqual(res2.coeffs, [1, 3, 5])
+
+    def test_addition_polynomial_with_int(self):
+        # Initialization
+        p = Polynomial([1, 2, 3])
+        i = 5
+
+        # Addition
+        res1 = p + i
+        res2 = i + p
+
+        self.assertEqual(res1.coeffs, [1, 2, 8])
+        self.assertEqual(res2.coeffs, [1, 2, 8])
+
+    def test_sub_two_polynomial(self):
+        # Initialization
+        p1 = Polynomial([1, 2, 3])
+        p2 = Polynomial([2, 2])
+        p3 = Polynomial([1, 2, 1])
+
+        # Subtraction
+        res1 = p1 - p2
+        res2 = p2 - p1
+
+        # Test 1
+        self.assertEqual(res1.coeffs, [1, 0, 1])
+        self.assertEqual(res2.coeffs, [-1, 0, -1])
+
+        # Subtraction
+        res1 = p1 - p3
+        res2 = p3 - p1
+
+        # Test 2
+        self.assertEqual(res1.coeffs, [2])
+        self.assertEqual(res2.coeffs, [-2])
+
+    def test_sub_polynomial_with_int(self):
+        # Initialization
+        p1 = Polynomial([1, 2, 3])
+        i = 3
+
+        # Subtraction
+        res1 = p1 - i
+        res2 = i - p1
+
+        self.assertEqual(res1.coeffs, [1, 2, 0])
+        self.assertEqual(res2.coeffs, [-1, -2, 0])
+
+    def test_mul_two_polynomialt(self):
+        # Initialization
+        p1 = Polynomial([1, 1, 3])
+        p2 = Polynomial([2, 2])
+        p3 = Polynomial([1, -1, 1])
+
+        # Multiplication
+        res1 = p1 * p2
+        res2 = p2 * p1
+
+        # Test 1
+        self.assertEqual(res1.coeffs, [2, 4, 8, 6])
+        self.assertEqual(res2.coeffs, [2, 4, 8, 6])
+
+        # Multiplication
+        res1 = p1 * p3
+        res2 = p3 * p1
+
+        # Test 2
+        self.assertEqual(res1.coeffs, [1, 0, 3, -2, 3])
+        self.assertEqual(res2.coeffs, [1, 0, 3, -2, 3])
+
+    def test_mul_polynomial_with_int(self):
+        # Initialization
+        p1 = Polynomial([1, 1, 3])
+        i = 2
+
+        # Multiplication
+        res1 = p1 * i
+        res2 = i * p1
+
+        self.assertEqual(res1.coeffs, [2, 2, 6])
+        self.assertEqual(res2.coeffs, [2, 2, 6])
+
+    def test_raise_add_with_incorrect_arg(self):
+        # Initialization
+        p = Polynomial([1, 1, 3])
+        fs = [2.0, "2.0", [2.0], (2.0)]
+        
+        for f in fs:
+            with self.assertRaises(TypeError):
+                p + f
+        for f in fs:
+            with self.assertRaises(TypeError):
+                f + p
+
+    def test_raise_sub_with_incorrect_arg(self):
+        # Initialization
+        p = Polynomial([1, 1, 3])
+        fs = [2.0, "2.0", [2.0], (2.0)]
+        
+        for f in fs:
+            with self.assertRaises(TypeError):
+                p - f
+        for f in fs:
+            with self.assertRaises(TypeError):
+                f - p
+
+    def test_raise_mul_with_incorrect_arg(self):
+        # Initialization
+        p = Polynomial([1, 1, 3])
+        fs = [2.0, "2.0", [2.0], (2.0)]
+        
+        for f in fs:
+            with self.assertRaises(TypeError):
+                p * f
+        for f in fs:
+            with self.assertRaises(TypeError):
+                f * p
 
 
 if __name__ == '__main__':
